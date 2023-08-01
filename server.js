ebd50fcdb639e20517bc784790be10b5e23680b6
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-08-01 14:49:03 +0300

    feat : retrieve message body from whatsapp

--- server.js
+++ server.js
@@ -4,34 +4,24 @@ require('dotenv').config();
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Find your Account SID and Auth Token at twilio.com/console
 // and set the environment variables. See http://twil.io/secure
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 const bodyParser = require('body-parser');
 const app = require('express')();
 
-app.use(bodyParser.json());
-app.post('/message', (req, res) => {
-	console.log({ request: req.body });
-	return res.json({
-		test: 'Working',
-	});
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/test', (req, res) => {
-	return res.json({
-		test: 'Working',
-	});
+app.post('/message', (req, res) => {
+	const whatsAppBody = req.body;
+
+	const message = whatsAppBody.Body;
+	const twiml = new MessagingResponse();
+	twiml.message(message);
+	res.type('text/xml').send(twiml.toString());
 });
 
 app.listen(8080, function () {
 	console.log('Server listening');
 });
-
-// client.messages
-// 	.create({
-// 		from: 'whatsapp:+14155238886',
-// 		body: 'Hello, there!',
-// 		to: 'whatsapp:+261346804717',
-// 	})
-// 	.then((message) => console.log(message.sid));

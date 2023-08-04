4dc11b305616bc6395cc45106ef17aacaf56835b
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-08-04 11:10:56 +0300

    feat : update open API completion

--- src/chat-gpt.js
+++ src/chat-gpt.js
@@ -1,9 +1,10 @@
 const axios = require('axios');
-const OPEN_AI_URL = 'https://api.openai.com/v1/chat/completions';
+const OPEN_AI_URL = process.env.OPEN_AI_URL;
+const messages = [];
 
 const configuration = {
 	// organization: 'org-1zKIMicMChW2FaauX2gVL8Gc',
-	apiKey: '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',
+	apiKey: process.env.OPEN_AI_API,
 };
 
 module.exports = () => {
@@ -12,15 +13,15 @@ module.exports = () => {
 		Authorization: `Bearer ${configuration.apiKey}`,
 	};
 
-	const chat = async (message) => {
+	const chat = async (newMessage) => {
+		const message = {
+			role: 'user',
+			content: newMessage,
+		};
+		messages.push(message);
 		const body = {
 			model: 'gpt-3.5-turbo',
-			messages: [
-				{
-					role: 'user',
-					content: message,
-				},
-			],
+			messages,
 		};
 		return await axios
 			.post(
@@ -33,7 +34,12 @@ module.exports = () => {
 				}
 			)
 			.then(({ data }) => {
+				messages.push(data.choices[0].message);
+				console.log(body);
 				return data.choices[0].message.content;
+			})
+			.catch((e) => {
+				throw e;
 			});
 	};
 

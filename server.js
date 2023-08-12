d4536b361bc008f0539fbe238217ee964f7f3831
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-08-12 11:37:37 +0300

    feat : add verification hooks

--- server.js
+++ server.js
@@ -5,9 +5,15 @@ const bodyParser = require('body-parser');
 const webhooksController = require('./src/controllers/webhooks.controller');
 const app = require('express')();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use('/message', webhooksController);
+app.get('/verify', async (req, res) => {
+	const queries = JSON.stringify(req.query).split(',');
+	let challenge = queries[1].split(':')[1];
+	challenge = challenge.replaceAll('"', '');
+	return res.end(challenge).status(200);
+});
 
 app.listen(process.env.PORT, function () {
 	console.log('Server listening');

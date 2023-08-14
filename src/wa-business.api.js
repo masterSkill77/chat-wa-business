28fc36d96b52e4da06697dd4897ae9e69d403e96
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-08-14 13:47:08 +0300

    fix : use business api for each business

--- src/wa-business.api.js
+++ src/wa-business.api.js
@@ -1,12 +1,12 @@
 'use strict';
 const axios = require('axios');
 const WA_API_URL = process.env.WA_API_URL;
-const WA_API_TOKEN = process.env.WA_API_TOKEN;
 
 module.exports = () => {
 	const sendMessage = async (from, to, message) => {
-		const url = `${WA_API_URL}/${from}/messages`;
+		const url = `${WA_API_URL}/${from.businessNumberID}/messages`;
 
+		const WA_API_TOKEN = from.businessApi;
 		const body = {
 			messaging_product: 'whatsapp',
 			to,

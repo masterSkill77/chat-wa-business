ad8b44ff3d2b420e86b50e4320772b263734b45e
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-08-14 13:47:59 +0300

    feat : configure the message routes and implementation

--- src/controllers/webhooks.controller.js
+++ src/controllers/webhooks.controller.js
@@ -1,25 +1,29 @@
 'use strict';
 
 const messageService = require('../services/message.service')();
+const accountService = require('../services/account.service');
 
 const webhooksController = require('express').Router();
-webhooksController.post('/', async (req, res) => {
+webhooksController.post('/:businessName', async (req, res) => {
 	const response = req.body.entry[0].changes[0].value;
+
+	const phone_number = response.metadata.display_phone_number;
+	const account = await accountService.getBusinessByPhoneNumber(phone_number);
+
 	if (response.messages) {
 		const recipient = response.messages[0].from;
 		const message = await messageService.handlemessage(
 			response.messages[0].text.body
 		);
-		await messageService.sendMessage('106442799216287', recipient, message);
+		await messageService.sendMessage(account, recipient, message);
 		return res.json(message).status(200);
 	}
 });
 
-webhooksController.get('/', async (req, res) => {
+webhooksController.get('/:businessName', async (req, res) => {
 	const queries = JSON.stringify(req.query).split(',');
 	let challenge = queries[1].split(':')[1];
 	challenge = challenge.replaceAll('"', '');
-	console.log(challenge);
 	return res.end(challenge).status(200);
 });
 
--- src/services/message.service.js
+++ src/services/message.service.js
@@ -7,11 +7,11 @@ module.exports = messageService = () => {
 	const handlemessage = async (message) => {
 		return message;
 	};
-	const chatWithGPT = async (to, message) => {
-		return await ChatGPT.chat(to, message);
+	const chatWithGPT = async (to, message, waAccount) => {
+		return await ChatGPT.chat(to, message, waAccount);
 	};
 	const sendMessage = async (from, to, message) => {
-		const returnFromGpt = await chatWithGPT(to, message);
+		const returnFromGpt = await chatWithGPT(to, message, from);
 		return await waBusinessApi.sendMessage(from, to, returnFromGpt);
 	};
 	return {
@@ -19,26 +19,3 @@ module.exports = messageService = () => {
 		sendMessage,
 	};
 };
-
-// const MessagingResponse = require('twilio').twiml.MessagingResponse;
-// const accountSid = process.env.TWILIO_ACCOUNT_SID;
-// const authToken = process.env.TWILIO_AUTH_TOKEN;
-// const client = require('twilio')(accountSid, authToken);
-// const ChatGPT = require('./src/chat-gpt');
-// app.get('/message', async (req, res) => {
-// 	return res.json(
-// 		await waBusinessApi.sendMessage('116756568172163', '261346804717', 'hi')
-// 	);
-// });
-
-// app.post('/message', async (req, res) => {
-// 	const whatsAppBody = req.body;
-
-// 	const message = whatsAppBody.Body;
-
-// 	const response = await ChatGPT().chat(message);
-
-// 	const twiml = new MessagingResponse();
-// 	twiml.message(response);
-// 	res.type('text/xml').send(twiml.toString());
-// });
